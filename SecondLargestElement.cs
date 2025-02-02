81110124e6e0da72fde6894f5afe9450ab6f71f1
chillasravani <118984444+chillasravani@users.noreply.github.com>
2025-02-03 00:18:48 +0530

    Add files via upload
    This program is to find second largest element in an array.

---
+++ SecondLargestElement.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*mespace ConsoleApp1_NGA
+{
+    internal class SecondLargestElement
+    {
+        public void SecondLargest()
+        {
+            Console.WriteLine("Enter the size of the Array:");
+            int n=Convert.ToInt32(Console.ReadLine());
+            int[] arr=new int[n];
+            for(int i = 0; i < n; i++) {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+
+
+            }
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(arr[i] + "  ");
+            }
+            Console.WriteLine();
+            int p = 0;
+            int vp = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (arr[i] > p)
+                {
+                    vp = p;
+                    p = arr[i];
+                }
+                else if (arr[i] > vp)
+                    vp = arr[i];
+                {
+
+                }
+            }
+            Console.WriteLine("Second Largest Element in the Array: " + vp);
+        }
+        public class ScdLargest
+        {
+            public static void Main(string[] args)
+            {
+                SecondLargestElement sl = new SecondLargestElement();
+                sl.SecondLargest();
+            }
+        }
+        
+    }
+}*/
