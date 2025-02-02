60fcf4af03a0890616dd54a73c69d4b128c40adb
chillasravani <118984444+chillasravani@users.noreply.github.com>
2025-02-03 00:22:45 +0530

    Add files via upload
    This Program used to find the Duplicate characters in the string 
    Example:
    Here, you can see we have passed "google" as a string, and we got a result as "og". It means these two characters (og)are duplicates in the word "google".
    
    and to find the Unique characters in the string
    
    Example:
    you can see the output as"gole". This means after removing duplicate characters from the word "google," we get the "gole" which are unique chars.

---
+++ PrintingDuplicates.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+namespace ConsoleApp1_NGA
+{
+    internal class PrintingDuplicates
+
+    {
+        public void Print(string s)
+        {
+            Console.WriteLine(s);
+            string t = "";
+            string d = "";
+            for (int i = 0; i < s.Length; i++)
+            {
+                char ch = s[i];
+                if (t.IndexOf(ch) !=-1)
+                {
+                    d = d + ch;
+                   
+                }
+
+                else
+                {
+                    t = t + ch;
+                }
+                
+            }
+            Console.WriteLine();
+            Console.WriteLine("Duplicate characters are : " + d);
+            Console.WriteLine("Unique characters are: " + t);
+
+        }
+        public class Program
+        {
+            public static void Main3(string[] args) { 
+            {
+                PrintingDuplicates p = new PrintingDuplicates();
+                p.Print("google");
+            }
+        }
+
+    }
+}*/
