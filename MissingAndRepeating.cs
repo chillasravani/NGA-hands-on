e11e89e0ca09fd6adcb0e0ae1df1395cb4204a3b
chillasravani <118984444+chillasravani@users.noreply.github.com>
2025-02-03 00:21:06 +0530

    Add files via upload
    This program used to find
     Missing and Repeating in an Array
    
    Given an unsorted array of size n. Array elements are in the range of 1 to n. One number from set {1, 2, …n} is missing and one number occurs twice in the array. Find these two numbers.
    
    Examples: 
    
    Input: arr[] = {3, 1, 3}
    Output: Missing = 2, Repeating = 3
    Explanation: In the array, 2 is missing and 3 occurs twice

---
+++ MissingAndRepeating.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1_NGA
+{
+    internal class MissingAndRepeating
+    {
+        static void MissingAndRepeat(int[] arr) { 
+        
+             Array.Sort(arr );
+
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if ((arr[i + 1] - arr[i]!=1) && arr[i + 1] > arr[i])
+                {
+                    int missing = arr[i + 1] - 1;
+                    Console.WriteLine("Missing Number is " + missing);
+                    break;
+                }
+                
+            }
+            for(int i = 0; i < arr.Length - 1; i++) { 
+                if (arr[i] == arr[i + 1])
+                {
+                    Console.WriteLine("Repeated Number is " + arr[i]);
+                }
+            }
+        }
+        public class MissAndRepeat
+        {
+            public static void Main2(string[] args)
+            {
+                int[] arr = { 1, 2, 9, 8, 5, 3, 4, 6, 5 };
+                MissingAndRepeat(arr);
+            }
+        }
+    }
+}
