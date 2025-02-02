b332e0e450021c8ce221b922fc644f37abaa7f38
chillasravani <118984444+chillasravani@users.noreply.github.com>
2025-02-03 00:23:36 +0530

    Add files via upload
    This Program is to
    Reverse each word of the sentence (string)
    
    example: "My name is vasu"      output->"yM eman si usav"

---
+++ ReverseWords.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1_NGA
+{
+    internal class ReverseWords
+    {
+        static string Reverse(string s)
+
+        {
+            {
+                string t = "";
+                for (int i = s.Length - 1; i >= 0; i--)
+                {
+                    t = t + s[i];
+                }
+                return t;
+
+            }
+        }
+        static void Reversewrds(string s)
+        {
+
+            string[] str = s.Split(' ');
+            string[] reversed = new string[str.Length];
+            for (int i = 0; i < str.Length; i++)
+            {
+                reversed[i] = Reverse(str[i]);
+            }
+            for (int i = 0; i < reversed.Length; i++)
+            {
+
+                Console.Write(reversed[i].ToString() + " ");
+
+            }
+        }
+
+        
+
+        public class Program
+        {
+            public static void Main4(string[] args)
+            {
+                string s = "Hello how are you";
+                Reversewrds(s);
+
+
+            }
+        }
+    }
+}
+
