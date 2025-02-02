313d26be275d3de35cfc86718267c57671c77415
chillasravani <118984444+chillasravani@users.noreply.github.com>
2025-02-03 00:20:09 +0530

    Add files via upload
    This program is to find 
    Maximum consecutive one’s (or zeros) in a binary array
    Given a binary array, find the count of a maximum number of consecutive 1s present in the array.
    
    Examples : 
    
    Input: arr[] = {1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1}
    Output: 4
    Explanation: The maximum number of consecutive 1’s in the array is 4 from index 8-11.

---
+++ LongestConsecutiveArray.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1_NGA
+{
+    internal class LongestConsecutiveArray
+    {
+
+        static void LongestSub(int[] arr)
+        {
+            int onecount = 0;
+            int zerocount = 0;
+            int zerooccur = 0;
+            int oneoccur = 0;
+
+            for (int i = 0; i<arr.Length-1;i++)
+            {
+
+                if (arr[i] == 1 )
+                {
+                    onecount++;
+                    oneoccur = Math.Max(onecount, oneoccur);
+                zerocount = 0;
+
+                }
+                else if (arr[i] == 0 )
+                {
+                    zerocount++;
+                    zerooccur=Math.Max(zerocount, zerooccur);
+                 onecount = 0;
+                }
+            else if (arr[arr.Length - 1] == 1)
+              {
+                    oneoccur++;
+              }
+              else
+              {
+                    zerooccur++;
+              }
+               
+               
+                
+                
+            }
+            if (oneoccur>zerooccur)
+            {
+                Console.WriteLine("Length of the consecutive ones is " + oneoccur);
+            }
+            else
+            {
+                Console.WriteLine("length of the consecutive zeroes is " + zerooccur);
+            }
+        }
+        
+        
+        public class LongestSubArray
+        {
+            public static void Main1(string[] args)
+            {
+                Console.WriteLine("enter the size of the array:");
+                int n = Convert.ToInt32(Console.ReadLine());
+                int[] arr = new int[n];
+                for (int i = 0; i < n; i++)
+                {
+                    arr[i] = Convert.ToInt32(Console.ReadLine());
+                }
+                Console.WriteLine();
+                for (int i = 0; i < n; i++)
+                {
+                    Console.Write(arr[i] + "  ");
+                }
+
+
+                //nsole.WriteLine("the size of longest Sub Array is:" + result);
+                LongestSub(arr);
+            }
+        }
+        
+    }
+}
